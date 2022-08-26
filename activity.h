d751a87bcf0c7d5ecc432054b2054d7f6d847d55
Pili Dávila <93629747+PiliDavila17@users.noreply.github.com>
2022-08-26 08:48:46 -0500

    Add files via upload

--- activity.h
+++ activity.h
@@ -1,8 +1,8 @@
 // =================================================================
 //
 // File: activity.h
-// Author:
-// Date:
+// Author: Maria del Pilar Davila Verduzco
+// Date: 27 de agosto de 2022
 //
 // =================================================================
 #ifndef ACTIVITY_H
@@ -14,10 +14,14 @@
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	O(n)
 // =================================================================
 unsigned int sumaIterativa(unsigned int n) {
-	return 0;
+	int suma=0;
+	for (int i=0; i<n; i++)
+		suma+=i;
+
+	return suma;
 }
 
 // =================================================================
@@ -26,22 +30,31 @@ unsigned int sumaIterativa(unsigned int n) {
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	T(0), T(1)=1, T(n)= 1+T(n-1)
 // =================================================================
 unsigned int sumaRecursiva(unsigned int n) {
-	return 0;
+	if(n==1){
+		return 1;
+	}
+	if(n==0 || n<=0){
+		return 0;
+	}
+	return n+sumaRecursiva(n-1);
 }
-
 // =================================================================
 // sumaDirecta. Calculate the sum from 1 to n with a mathematical
 // method.
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	O(n^2)
 // =================================================================
+// n(n+1)/2
 unsigned int sumaDirecta(unsigned int n) {
-	return 0;
+	if (n==1)
+		return 1;
+	 
+	return (n*(n+1)/2);
 }
 
 #endif /* ACTIVITY_H */
